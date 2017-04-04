af331e275502d81785d2c868ed5c91b3d36fec65
davidsodaa <sodakolor@gmail.com>
2017-04-04 09:00:45 +0200

    Procfile

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -183,7 +183,7 @@ function initMap() {
 }
 
 $(document).ready(() => {
-  $('.navbar a').click(function(e){
+  $('.navbar a,#linebreak a').click(function(e){
       let to = $(this).attr('href');
       $('html, body').animate({
         scrollTop: $(to).offset().top
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(express.static('public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));  
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); 
+app.use('/fonts/', express.static(path.join(__dirname, '/node_modules/bootstrap/fonts')));
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
